0131efaaff1d6dbdc39ae1b886bdf9a80c663249
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2026-06-09 10:48:37 +0800

    add github action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/SmartWeb",
+  assetPrefix: "/SmartWeb",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
